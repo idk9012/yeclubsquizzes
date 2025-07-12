8ff26c9437ab1bb8484c33b7eb04356691a1d2fa
idk9012 <79174514+idk9012@users.noreply.github.com>
2025-07-12 11:43:43 +0300

    Add files via upload

---
+++ check_submission_detailed.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+const Submission = require('./server/models/Submission');
+
+mongoose.connect(process.env.MONGO_URI).then(async () => {
+  const submission = await Submission.findOne({ 
+    quizId: '68700b38fdee775eaa892829',
+    isCompleted: true 
+  }).sort({ submittedAt: -1 });
+  
+  console.log('Submission found:', !!submission);
+  if (submission) {
+    console.log('Score:', submission.score);
+    console.log('Answers count:', submission.answers.length);
+    console.log('Full submission answers:', JSON.stringify(submission.answers, null, 2));
+  }
+  process.exit(0);
+}).catch(err => {
+  console.error('Error:', err);
+  process.exit(1);
+});
+
